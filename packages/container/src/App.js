5b0f2d962e88261827262b54c4c539320bfe546e
logheadloghead <63432718+logheadloghead@users.noreply.github.com>
2023-12-30 16:23:59 -0500

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { BrowserRouter  } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp';
+import Progress from './components/Progress';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -15,7 +18,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
